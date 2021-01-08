072846f1515961326f1c3c7d207e19df63d9f48d
Yesterday17 <t@yesterday17.cn>
2021-01-08 21:16:41 +0800

    Sort paths in walk path.

--- anni/src/fs.rs
+++ anni/src/fs.rs
@@ -6,9 +6,10 @@ pub use std::fs::metadata;
 fn fs_walk_path(path: PathBuf, recursive: bool, callback: &impl Fn(PathBuf) -> bool) -> io::Result<bool> {
     let meta = metadata(&path)?;
     if meta.is_dir() && recursive {
-        let dir = read_dir(path)?;
+        let mut dir: Vec<_> = read_dir(path)?.map(|r| r.unwrap().path()).collect();
+        dir.sort();
         for entry in dir {
-            if !fs_walk_path(entry?.path(), recursive, callback)? {
+            if !fs_walk_path(entry, recursive, callback)? {
                 return Ok(false);
             }
         }
