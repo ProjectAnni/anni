fb0b94b9a63a7f98cb7ea360561b337a90d826a2
Yesterday17 <mmf@mmf.moe>
2021-06-20 12:31:02 +0800

    anni(vgmdb): print valid toml format

--- anni/src/subcommands/get.rs
+++ anni/src/subcommands/get.rs
@@ -39,12 +39,19 @@ impl Subcommand for GetSubcommand {
 fn vgmdb_search(catalog: &str) -> anyhow::Result<()> {
     let client = VGMClient::new();
     let album = block_on(client.album(catalog))?;
+    println!("[album]");
     println!(r#"title = "{}""#, album.name());
-    println!(r#"date = {}"#, album.release_date.as_deref().unwrap_or("Unknown"));
+    println!(r#"artist = """#);
+    println!(r#"date = {}"#, album.release_date.as_deref().unwrap_or("0000-00-00"));
+    println!(r#"type = "normal""#);
     println!(r#"catalog = "{}""#, album.catalog());
+    println!();
 
     for disc in album.discs() {
-        println!("[[discs]]\n");
+        println!("[[discs]]");
+        println!(r#"catalog = "{}""#, album.catalog());
+        println!();
+
         for track in disc.tracks() {
             println!("[[discs.tracks]]");
             println!(r#"title = "{}""#, track.name());
