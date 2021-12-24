6088982ade2b5ae7bb1254857a1b5bb26455233d
Yesterday17 <mmf@mmf.moe>
2021-12-24 14:22:56 +0800

    anni(repo): make repo db fields public

--- anni-repo/src/db/rows.rs
+++ anni-repo/src/db/rows.rs
@@ -3,40 +3,40 @@ use uuid::Uuid;
 
 #[derive(FromRow, Debug)]
 pub struct AlbumRow {
-    album_id: Uuid,
-    title: String,
-    edition: Option<String>,
-    catalog: String,
-    artist: String,
-    release_date: String,
-    album_type: String,
+    pub album_id: Uuid,
+    pub title: String,
+    pub edition: Option<String>,
+    pub catalog: String,
+    pub artist: String,
+    pub release_date: String,
+    pub album_type: String,
 }
 
 #[derive(FromRow, Debug)]
 pub struct DiscRow {
-    album_id: Uuid,
-    disc_id: u8,
-    title: String,
-    artist: String,
-    catalog: String,
-    disc_type: String,
+    pub album_id: Uuid,
+    pub disc_id: u8,
+    pub title: String,
+    pub artist: String,
+    pub catalog: String,
+    pub disc_type: String,
 }
 
 #[derive(FromRow, Debug)]
 pub struct TrackRow {
-    album_id: Uuid,
-    disc_id: u8,
-    track_id: u8,
-    title: String,
-    artist: String,
-    track_type: String,
+    pub album_id: Uuid,
+    pub disc_id: u8,
+    pub track_id: u8,
+    pub title: String,
+    pub artist: String,
+    pub track_type: String,
 }
 
 #[derive(FromRow, Debug)]
 pub struct TagRow {
-    album_id: Uuid,
-    disc_id: Option<u8>,
-    track_id: Option<u8>,
-    name: String,
-    edition: Option<String>,
+    pub album_id: Uuid,
+    pub disc_id: Option<u8>,
+    pub track_id: Option<u8>,
+    pub name: String,
+    pub edition: Option<String>,
 }
