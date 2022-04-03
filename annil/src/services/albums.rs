36da9815c384edeb54613b9f9a384314c4320532
Yesterday17 <mmf@mmf.moe>
2022-04-03 18:28:14 +0800

    feat(annil): support weak tag

--- annil/src/services/albums.rs
+++ annil/src/services/albums.rs
@@ -9,7 +9,10 @@ use crate::{AnnilClaims, AppState};
 async fn albums(claims: AnnilClaims, data: web::Data<AppState>, req: HttpRequest) -> impl Responder {
     match claims {
         AnnilClaims::User(_) => {
-            if let Some(Ok(etag)) = req.headers().get(IF_NONE_MATCH).map(|v| v.to_str()) {
+            if let Some(Ok(mut etag)) = req.headers().get(IF_NONE_MATCH).map(|v| v.to_str()) {
+                if etag.starts_with("W/") {
+                    etag = &etag[2..];
+                }
                 if etag == data.etag.read().as_str() {
                     return HttpResponse::NotModified().finish();
                 }
