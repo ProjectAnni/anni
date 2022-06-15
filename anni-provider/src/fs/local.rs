67be312369b1a5dce42ea8793624fadbdfa7ddd2
Yesterday17 <mmf@mmf.moe>
2022-06-15 16:19:02 +0800

    refactor(provider): local FileSystemProvider

--- anni-provider/src/fs/local.rs
+++ anni-provider/src/fs/local.rs
@@ -1,46 +1,51 @@
-use std::future::Future;
 use std::io::SeekFrom;
 use std::path::PathBuf;
 use std::pin::Pin;
-use std::task::Poll;
 use async_trait::async_trait;
+use tokio::fs::read_dir;
 use tokio::io::{AsyncReadExt, AsyncSeekExt};
-use tokio_stream::Stream;
-use crate::{FileEntry, FileSystemProvider, Range, ResourceReader};
+use tokio_stream::{Stream, self as stream};
+use crate::{FileEntry, FileSystemProvider, ProviderError, Range, ResourceReader};
 
-struct FutureWrapper<T: Clone>(T);
-
-impl<T: Clone> Future for FutureWrapper<T> {
-    type Output = T;
-
-    fn poll(self: Pin<&mut Self>, _: &mut std::task::Context<'_>) -> std::task::Poll<Self::Output> {
-        Poll::Ready(self.0.clone())
-    }
-}
-
-pub struct LocalFileSystemProvider {}
+pub struct LocalFileSystemProvider;
 
 #[async_trait]
 impl FileSystemProvider for LocalFileSystemProvider {
     async fn children(&self, path: &PathBuf) -> crate::Result<Pin<Box<dyn Stream<Item=FileEntry> + Send>>> {
-        todo!()
-        // Ok(Box::new(
-        //     std::fs::read_dir(path)?
-        //         .filter_map(|entry| {
-        //             let entry = entry.unwrap();
-        //             let path = entry.path();
-        //             let name = path.file_name().unwrap().to_string_lossy().to_string();
-        //             if path.is_dir() {
-        //                 Some(wrap(FileEntry { name, path }))
-        //             } else {
-        //                 None
-        //             }
-        //         })
-        // ))
+        Ok(Box::pin(stream::iter(
+            std::fs::read_dir(path)?
+                .filter_map(|entry| {
+                    let entry = entry.ok()?;
+                    let path = entry.path();
+                    let name = path.file_name()?.to_string_lossy().to_string();
+                    if path.is_dir() {
+                        Some(FileEntry { name, path })
+                    } else {
+                        None
+                    }
+                })
+        )))
     }
 
     async fn get_file_entry_by_prefix(&self, parent: &PathBuf, prefix: &str) -> crate::Result<FileEntry> {
-        todo!()
+        let mut dir = read_dir(parent).await?;
+        loop {
+            match dir.next_entry().await? {
+                Some(entry)
+                if entry
+                    .file_name()
+                    .to_string_lossy()
+                    .starts_with(prefix) =>
+                    {
+                        return Ok(FileEntry {
+                            name: entry.file_name().to_string_lossy().to_string(),
+                            path: entry.path(),
+                        });
+                    }
+                None => return Err(ProviderError::FileNotFound),
+                _ => {}
+            }
+        };
     }
 
     async fn get_file(&self, path: &PathBuf, range: Range) -> crate::Result<ResourceReader> {
