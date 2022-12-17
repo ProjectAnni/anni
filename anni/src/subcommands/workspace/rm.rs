44a10acbd33d2f7cbe1f4ad99b645a5102b60a15
Yesterday17 <mmf@mmf.moe>
2022-12-17 18:23:56 +0800

    refactor(anni): change semantics of anni rm: revert album status instead of removing album directly

--- anni/src/subcommands/workspace/rm.rs
+++ anni/src/subcommands/workspace/rm.rs
@@ -1,16 +1,43 @@
 use crate::workspace::utils::{do_get_album_id, find_dot_anni, get_workspace_album_path};
-use anni_common::fs::remove_dir_all;
+use anni_common::fs::{self, remove_dir_all};
 use clap::Args;
 use clap_handler::handler;
 use inquire::Confirm;
-use std::path::PathBuf;
+use std::fs::create_dir_all;
+use std::path::{Path, PathBuf};
 
+/// Revert workspace album back to uncommitted state
 #[derive(Args, Debug, Clone)]
 pub struct WorkspaceRmAction {
     #[clap(short = 'y', long = "yes")]
     skip_check: bool,
 
     path: PathBuf,
+}
+
+fn recover_symlinks<P: AsRef<Path>>(path: P) -> anyhow::Result<()> {
+    log::debug!("Recovering path: {}", path.as_ref().display());
+    let metadata = fs::symlink_metadata(path.as_ref())?;
+    if metadata.is_symlink() {
+        // ignore .album directories
+        if let Some(file_name) = path.as_ref().file_name() {
+            if file_name == ".album" {
+                return Ok(());
+            }
+        }
+
+        // copy pointing file to current path
+        let actual_path = fs::canonicalize(path.as_ref())?;
+        log::debug!("Actual path: {}", actual_path.display());
+        fs::rename(actual_path, path)?;
+    } else if metadata.is_dir() {
+        for entry in path.as_ref().read_dir()? {
+            let entry = entry?;
+            recover_symlinks(entry.path())?;
+        }
+    }
+
+    Ok(())
 }
 
 #[handler(WorkspaceRmAction)]
@@ -30,7 +57,10 @@ pub async fn handle_workspace_rm(me: WorkspaceRmAction) -> anyhow::Result<()> {
         }
     }
 
-    remove_dir_all(me.path, true)?;
+    recover_symlinks(me.path)?;
+
+    // remove and re-create album path
     remove_dir_all(&album_path, true)?;
+    create_dir_all(&album_path)?;
     Ok(())
 }
